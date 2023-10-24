5ddf30de41860c2eb480af7f2f56f3b41261e97e
Jose Suarez <josysuarez2003@gmail.com>
2023-10-24 20:29:23 +0200

    cambios

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
 import requests
 
 app = Flask(__name__)
 app.static_folder = 'static'
+app.secret_key = 'your_secret_key'
 
-@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        session['user_name'] = name
+        return redirect(url_for('lobby'))
     return render_template('index.html')
 
 @app.route('/lobby', methods=['GET', 'POST'])
 def lobby():
-    return render_template('lobby.html')
+    user_name = session.get('user_name')
+    return render_template('lobby.html', user_name=user_name)
+
+@app.route('/save-name', methods=['POST'])
+def save_name():
+    data = request.get_json()
+    name = data['name']
+    session['user_name'] = name
+    return jsonify({'status': 'ok'}), 200
 
 @app.route('/playa-escena', methods=['GET','POST'])
 def playa():
--- static/js/nombre.js
+++ static/js/nombre.js
@@ -1,4 +1,25 @@
 document.getElementById('nameForm').addEventListener('submit', function(event){
     event.preventDefault(); // prevent form from submitting normally
     var name = document.getElementById("name").value;
+
+    var regex_nombre = /^[a-zA-Z\s]*$/;
+    if (!regex_nombre.test(name)){
+        const regex_error = document.getElementById('regex');
+        regex_error.innerHTML ="<p>Solo se permite caracteres y espacios.</p>";
+    }
+
+    fetch('/save-name', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name:name})
+    }).then(function(response){
+        if(response.ok){
+            window.location.href = '/lobby';
+        } else{
+            const error = document.getElementById('error');
+            error.innerHTML = "<p>Error en guardar el nombre.</p>"
+        }
+    })
 });
