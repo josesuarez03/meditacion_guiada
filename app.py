c99d680a43f071196ad5a4265290eb44357a969e
Jose Suarez <josysuarez2003@gmail.com>
2023-10-26 11:30:33 +0200

    update

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, session
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session, abort
 import requests
+from functools import wraps
 
 app = Flask(__name__)
 app.static_folder = 'static'
 app.secret_key = 'your_secret_key'
+
+def auth_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        if 'user_name' not in session:
+            return render_template('403.html'), 403
+        return func(*args, **kwargs)
+    return decorated
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -14,6 +23,7 @@ def index():
     return render_template('index.html')
 
 @app.route('/lobby', methods=['GET', 'POST'])
+@auth_required
 def lobby():
     user_name = session.get('user_name')
     return render_template('lobby.html', user_name=user_name)
@@ -26,16 +36,27 @@ def save_name():
     return jsonify({'status': 'ok'}), 200
 
 @app.route('/playa-escena', methods=['GET','POST'])
+@auth_required
 def playa():
     return render_template('playa.html')
 
 @app.route('/templo-zen-escena',methods=['GET','POST'])
+@auth_required
 def templo_zen():
     return render_template('templo_zen.html')
 
 @app.route('/bosque-escena',methods=['GET','POST'])
+@auth_required
 def bosque():
     return render_template('bosque.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def error_server():
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
