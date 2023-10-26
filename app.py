5bd1a31975d3670fc074c389a198f626e4c6665d
Jose Suarez <josysuarez2003@gmail.com>
2023-10-26 11:49:37 +0200

    cambios

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, session, abort
-import requests
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
 from functools import wraps
+from datetime import timedelta
 
 app = Flask(__name__)
 app.static_folder = 'static'
 app.secret_key = 'your_secret_key'
+
+app.permanent_session_lifetime = timedelta(minutes=30)
 
 def auth_required(func):
     @wraps(func)
@@ -15,6 +17,7 @@ def auth_required(func):
     return decorated
 
 @app.route('/', methods=['GET','POST'])
+@auth_required
 def index():
     if request.method == 'POST':
         name = request.form.get('name')
@@ -27,6 +30,11 @@ def index():
 def lobby():
     user_name = session.get('user_name')
     return render_template('lobby.html', user_name=user_name)
+
+@app.route('/logout')
+def logout():
+    session.pop('user_name', None)
+    return redirect(url_for('index'))
 
 @app.route('/save-name', methods=['POST'])
 def save_name():
@@ -55,8 +63,12 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
-def error_server():
+def error_server(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
+    # Limpia las sesiones al iniciar el servidor
+    app.before_request(lambda: session.modified and session.pop('user_name', None))
+
     app.run(debug=True)
+
