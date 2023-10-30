bd50a7d66da1ff88ddb1b99865a3549123472651
Jose Suarez <josysuarez2003@gmail.com>
2023-10-30 13:12:04 +0100

    update

--- static/js/genero.js
+++ static/js/genero.js
@@ -1,5 +1,5 @@
 async function obtenerGeneroNom(nombre){
-    var api = 'https://api.genderize.io?name=' + nombre;
+    var api = `https://gender-api.com/get?name=${nombre}&key=kqkGxSyE6MZTuXD77CAFxrPVTfUpVcRtSwaS`;
     try {
         const response = await fetch(api);
         const data = await response.json();
@@ -10,6 +10,27 @@ async function obtenerGeneroNom(nombre){
     }
 }
 
-function saludo(){
-    
+async function saludo() {
+    var msj = document.getElementById('mensaje');
+    var user_name = msj.getAttribute('data-user-name');
+
+    try {
+        var genero = await obtenerGeneroNom(user_name);
+
+        if (genero === 'male') {
+            msj.setAttribute('value', 'Bienvenido ' + user_name + ', seleccione una de las escenas.');
+        } 
+        else if (genero === 'female') {
+            msj.setAttribute('value', 'Bienvenida ' + user_name + ', seleccione una de las escenas.');
+        } 
+        else {
+            msj.setAttribute('value', 'Bienvenido/a ' + user_name + ', seleccione una de las escenas.');
+        }
+    } catch (error) {
+        console.error('Error al obtener el género: ', error);
+    }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    saludo();
+});
